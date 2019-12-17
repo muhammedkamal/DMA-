dc4bd1c624b421ac9f7fcd084d9036d301e6fc12
unknown <muhammedkamal022@gmail.com>
2019-12-17 17:24:33 +0200

    all tested successfully 2 :)

--- iodevice2.v
+++ iodevice2.v
@@ -1,10 +1,10 @@
-module IODevice (Ack1,GPIO1,Data,IOWrite1,clk,index);
-input IOWrite1; 
-input Ack1;
+module IODevice2 (Ack2,GPIO2,Data,IOWrite2,clk,index);
+input IOWrite2; 
+input Ack2;
 input wire [8:0] index;
 wire CS = index[8];
 integer bufferind;
-output reg GPIO1;
+output reg GPIO2;
 //reg GP ;
 inout[31:0] Data ;
 reg [31:0] OData;
@@ -18,8 +18,8 @@ reg interrupt [0:1];
 reg [31:0] BufferReg [0:30];
 reg [7:0] StatusReg;
 integer count;
-assign Data = (IOWrite1)?32'bzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz : OData ; //if read Data
-//assign GPIO1 = GP;
+assign Data = (IOWrite2)?32'bzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz : OData ; //if read Data
+//assign GPIO2 = GP;
 
 
 initial 
@@ -40,14 +40,14 @@ always@(clk)
 begin
 // checking interrupt by gui 
 count=0;
-$readmemb("interrupt1.mem", interrupt);
+$readmemb("interrupt2.mem", interrupt);
 if (interrupt[0])
 begin 
-assign GPIO1 = 1;
+assign GPIO2 = 1;
 i=0;
-//$monitor("%b" ,GPIO1);
+//$monitor("%b" ,GPIO2);
 //read buffermemory 
-$readmemh("buffermemory1.mem", BufferReg);
+$readmemh("buffermemory2.mem", BufferReg);
 //check number of words at memory 
 for(i=0;i<31;i=i+1)
 begin
@@ -66,22 +66,22 @@ end
 bufferind = index[7:0]-192;
 if (CS ==1)
 begin
-if(IOWrite1 == 1) //write
+if(IOWrite2 == 1) //write
 begin 
 //to store in buffer
 //GP <= 1;
  BufferReg[bufferind] <= Data ;
 end
-if(IOWrite1 == 0) //write
+if(IOWrite2 == 0) //write
 begin 
 //to store in buffer
 //GP <= 1;
  OData <= BufferReg[bufferind] ;
 end
 end
-if (Ack1 ==1)
+if (Ack2 ==1)
 begin
-if(IOWrite1 == 1) //write
+if(IOWrite2 == 1) //write
 begin 
 //to store in buffer
 //GP <= 1;
@@ -90,7 +90,7 @@ begin
 
 end
 
-if (!IOWrite1) begin // read
+if (!IOWrite2) begin // read
 
  if(count>0)
  begin
@@ -103,19 +103,19 @@ begin
 
  OData = BufferReg[i] ;
  BufferReg[i] = 0 ;
-$writememh("buffermemory1.mem", BufferReg);
+$writememh("buffermemory2.mem", BufferReg);
  count = count-1;
 if (count==32'h00000000)
 begin
-assign GPIO1 = 0;
+assign GPIO2 = 0;
 interrupt[0]=0;
-$writememb("interrupt1.mem", interrupt);
+$writememb("interrupt2.mem", interrupt);
 end
 end
 
 // GP <= 0;
 /*count=0;
-GPIO1 =0;
+GPIO2 =0;
 interrupt[0]=0;
 $writememb("interrupt.mem", interrupt);*/
  end
@@ -130,17 +130,17 @@ end
 
 endmodule
 //test for intruptting and count 
-module newtest1();
+module newtest2();
 
 wire [31:0] Data;
-reg Ack1;
-reg IOWrite1;
-wire GPIO1;
+reg Ack2;
+reg IOWrite2;
+wire GPIO2;
 reg clk;
 reg [8:0] index;
 reg [31:0]inData ;
 
-assign Data = (IOWrite1)? inData :32'bzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz;
+assign Data = (IOWrite2)? inData :32'bzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz;
 initial clk=1;
 
 always #5 clk=~clk;
@@ -151,83 +151,86 @@ assign IOWrite2 =0;
 assign index = 448;
 $monitor("%b %d %d ",index, IOWrite2 ,Data);
 end*/
-IODevice2 dev(Ack1,GPIO1,Data,IOWrite1,clk,index);
+IODevice2 dev(Ack2,GPIO2,Data,IOWrite2,clk,index);
 initial
 begin
-assign Ack1=0;
-assign IOWrite1 =0;
+assign Ack2=0;
+assign IOWrite2 =0;
 assign index = 448;
-$monitor("%b %d %d ",index, IOWrite1 ,Data);
+$monitor("%b %d %d ",index, IOWrite2 ,Data);
 #8
-assign Ack1=1;
-assign IOWrite1 =0;
+assign Ack2=1;
+assign IOWrite2 =0;
 assign index = 192;
-$monitor("%b %d %d ",index, IOWrite1 ,Data);
+$monitor("%b %d %d ",index, IOWrite2 ,Data);
 #8
-assign Ack1=1;
-assign IOWrite1 =1;
+assign Ack2=1;
+assign IOWrite2 =1;
 assign index = 192;
 assign inData =5;
-$monitor("%b %d %d ",index, IOWrite1 ,Data);
+$monitor("%b %d %d ",index, IOWrite2 ,Data);
 end
 
-endmodule  
+
+
+
+endmodule 
 module testbench();
 
 wire [31:0] Data;
-reg IOWrite1;
-wire GPIO1;
+reg IOWrite2;
+wire GPIO2;
 reg inData ;
 
-assign Data = (IOWrite1)? inData :32'bzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz;
+assign Data = (IOWrite2)? inData :32'bzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz;
 
 initial 
 begin
 #6
-IOWrite1 = 1'b1;
+IOWrite2 = 1'b1;
 inData =32'b1 ;
-$monitor("%d %d %d  ",GPIO1, IOWrite1 ,Data );
+$monitor("%d %d %d  ",GPIO2, IOWrite2 ,Data );
 #6
-IOWrite1 = 1'b1;
+IOWrite2 = 1'b1;
 inData = 32'b0 ;
-$monitor("%d %d %d ",GPIO1, IOWrite1 ,Data);
+$monitor("%d %d %d ",GPIO2, IOWrite2 ,Data);
 #6
-IOWrite1 = 1'b1;
+IOWrite2 = 1'b1;
 inData = 32'b1;
-$monitor("%d %d %d  ",GPIO1, IOWrite1 ,Data );
+$monitor("%d %d %d  ",GPIO2, IOWrite2 ,Data );
 
 #6
-IOWrite1 = 1'b1;
+IOWrite2 = 1'b1;
 inData = 32'b0;
-$monitor("%d %d %d  ",GPIO1, IOWrite1 ,Data );
+$monitor("%d %d %d  ",GPIO2, IOWrite2 ,Data );
 
 #6
-IOWrite1 = 1'b1;
+IOWrite2 = 1'b1;
 inData = 32'b1;
-$monitor("%d %d %d  ",GPIO1, IOWrite1 ,Data );
+$monitor("%d %d %d  ",GPIO2, IOWrite2 ,Data );
 
 
 
 #6
-IOWrite1 = 1'b0;
-$monitor("%d %d %d  ",GPIO1, IOWrite1 ,Data );
+IOWrite2 = 1'b0;
+$monitor("%d %d %d  ",GPIO2, IOWrite2 ,Data );
 #6
-IOWrite1 = 1'b1;
+IOWrite2 = 1'b1;
 inData = 32'b1;
-$monitor("%d %d %d  ",GPIO1, IOWrite1 ,Data );
+$monitor("%d %d %d  ",GPIO2, IOWrite2 ,Data );
 
 
 /*
 #6
-IOWrite1 = 1'b0;
-$monitor("%d %d %d  ",GPIO1, IOWrite1 ,Data );
+IOWrite2 = 1'b0;
+$monitor("%d %d %d  ",GPIO2, IOWrite2 ,Data );
 #6
-IOWrite1 = 1'b0;
-$monitor("%d %d %d  ",GPIO1, IOWrite1 ,Data );
+IOWrite2 = 1'b0;
+$monitor("%d %d %d  ",GPIO2, IOWrite2 ,Data );
 */
 end
 
-IODevice device(GPIO1,Data,IOWrite1,clock );
+IODevice2 device(GPIO2,Data,IOWrite2,clock );
 clkgenertor c1(clock);
 
 
@@ -244,9 +247,9 @@ clock = ~clock;
 end 
 endmodule 
 
-module interrupt_test(n,GPIO1, Data ,inData , IOWrite1);
-input IOWrite1; 
-output reg GPIO1;
+module interrupt_test(n,GPIO2, Data ,inData , IOWrite2);
+input IOWrite2; 
+output reg GPIO2;
 output reg [31:0] Data;
 
 input [31:0] inData ;
@@ -256,11 +259,11 @@ reg interrupt [0:1];
 always 
 begin 
 #6
-$readmemb("interrupt1.mem", interrupt);
+$readmemb("interrupt2.mem", interrupt);
 if (interrupt[0])
 begin 
-assign GPIO1 = 1;
-$monitor("%b" ,GPIO1);
+assign GPIO2 = 1;
+$monitor("%b" ,GPIO2);
 $readmemh("memory1.mem", BufferReg);
 end 
 end 
